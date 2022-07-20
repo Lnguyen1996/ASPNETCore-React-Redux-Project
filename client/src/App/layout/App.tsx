9356028cd7c0d6d2cd4fb52131932d53158487c6
Lam Nguyen <lnguyen4e@gmail.com>
2022-07-20 09:54:12 -0400

    section 3

---
+++ client/src/App/layout/App.tsx
@@ -0,0 +1,30 @@
+import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { Container } from "@mui/system";
+import { useState } from "react";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+function App() {
+  const [darkMode,setDarkMode] = useState(false);
+  const paletteType = darkMode?'dark':'light';
+  const theme = createTheme({
+    palette:{
+      mode:paletteType,
+      background:{
+        default:paletteType==='light'?'#eaeaea':'#121212'
+      }
+    }
+  })
+
+  return (
+    <ThemeProvider theme={theme} >
+      <CssBaseline />
+      <Header isDarkMode={darkMode} setDarkMode={()=>setDarkMode(!darkMode)}/>
+      <Container>
+        <Catalog  />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
